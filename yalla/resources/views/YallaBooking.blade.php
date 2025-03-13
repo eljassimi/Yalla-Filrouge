c0387b6815c09d82d15af415a82a8ac6db18b5a1
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-03-13 22:55:32 +0000

    create the first card static that display the details of the hotel

--- yalla/resources/views/YallaBooking.blade.php
+++ yalla/resources/views/YallaBooking.blade.php
@@ -112,6 +112,36 @@
     </div>
 </div>
 
+<!-- Main Content -->
+<main class="flex-1  py-6 px-6">
+    <div class="max-w-6xl mx-auto">
 
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <!-- Card 1 -->
+            <div class="shadow-xl rounded-lg overflow-hidden  transform transition-transform hover:-translate-y-2">
+                <div class="h-56 bg-gray-800 relative">
+                    <img src="/assets/stay/stay3.png" alt="Mountain View Villa" class="w-full h-full object-cover">
+                </div>
+                <div class="p-4">
+                    <div class="flex justify-between items-start mb-1 text-darkgray">
+                        <h3 class="text-lg font-bold">Mountain View Villa</h3>
+                    </div>
+                    <p class="text-sm text-darkgray mb-2">Chefchaouen, Morocco</p>
+                    <p class="text-sm mb-4 text-darkgray">Charming 4-bedroom villa with garden and Atlas Mountains view</p>
+                    <div class="flex justify-between items-center">
+                        <div>
+                            <span class="font-bold text-darkgray text-lg">300 DH</span>
+                            <span class="text-darkgray">/night</span>
+                        </div>
+                        <button class="bg-primary hover:bg-primary/90 text-white px-4 py-1.5 text-sm font-medium rounded-md transition">
+                            Details
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</main>
 </body>
 </html>
