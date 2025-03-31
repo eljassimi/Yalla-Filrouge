ec25a64e5f82731153beb835e3982fa4f687d950
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-03-31 23:41:26 +0000

    create the transport controller

---
+++ yalla/app/Http/Controllers/TransportController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\Mailer\Transport;
+use App\Models\TransportService;
+
+class TransportController extends Controller
+{
+
+}
