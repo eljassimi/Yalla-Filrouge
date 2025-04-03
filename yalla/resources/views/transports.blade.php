3c43c2827a837a72611355716074210e0da2f62d
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-03 17:19:25 +0000

    remplace the head in the transports  page with the new component

--- yalla/resources/views/transports.blade.php
+++ yalla/resources/views/transports.blade.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" class="dark">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Yalla Booking - Transport</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <script>
-        tailwind.config = {
-            darkMode: 'class',
-            theme: {
-                extend: {
-                    fontFamily: {
-                        'poppins': ['Poppins', 'sans-serif'],
-                    },
-                    colors: {
-                        primary: '#A22C29',
-                        darkgray: '#1F1F1F',
-                        lightgray: '#B9BAA3',
-                    }
-                }
-            }
-        }
-    </script>
-    <style>
-        body {
-            font-family: 'Poppins', sans-serif;
-        }
-    </style>
-</head>
+<x-head />
 <body class="min-h-screen flex flex-col text-white font-['Poppins']">
 <!-- Header -->
 <x-navbar />
