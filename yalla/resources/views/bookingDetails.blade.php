abc8acddf1d379ebbf5933bd19d63b5b12f45e63
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-03 17:18:46 +0000

    remplace the head in the booking details  page with the new component

--- yalla/resources/views/bookingDetails.blade.php
+++ yalla/resources/views/bookingDetails.blade.php
@@ -1,30 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Yalla - Moroccan Riad Booking</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
-            darkMode: 'class',
-            theme: {
-                extend: {
-                    fontFamily: {
-                        'poppins': ['Poppins', 'sans-serif'],
-                    },
-                    colors: {
-                        primary: '#A22C29',
-                        darkgray: '#1F1F1F',
-                        lightgray: '#B9BAA3',
-                    }
-                }
-            }
-        }
-    </script>
-</head>
+<x-head />
 <body class="min-h-screen bg-[#fbfbfb] flex flex-col">
-
 <x-navbar />
 <main class="flex-1 container mx-auto px-4 py-6">
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
