d3fc8d771c87502a05af2580f5ed900e86ec05fd
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-07 12:12:15 +0100

    add a decorations elements

---
+++ yalla/resources/views/checkout.blade.php
@@ -0,0 +1,12 @@
+<x-head />
+<body class="bg-darkgray text-white font-poppins min-h-screen">
+<x-navbar />
+
+<!-- Payment Section -->
+<section class="py-12 md:py-20 relative overflow-hidden">
+    <!-- Decorative elements -->
+    <div class="absolute top-0 left-0 w-48 h-48 bg-primary/10 rounded-full -translate-x-1/2 -translate-y-1/2 blur-3xl"></div>
+    <div class="absolute bottom-0 right-0 w-64 h-64 bg-primary/10 rounded-full translate-x-1/2 translate-y-1/2 blur-3xl"></div>
+</section>
+</body>
+</html>
