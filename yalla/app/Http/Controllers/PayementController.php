647ab833398f0d77d7a2b73264640f7aeb9843f3
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-07 14:42:45 +0100

    calculate the total price of the user selctions

--- yalla/app/Http/Controllers/PayementController.php
+++ yalla/app/Http/Controllers/PayementController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TicketType;
 use App\Models\userSelections;
 use Illuminate\Http\Request;
 
 class PayementController extends Controller
 {
-    public function index(){
+    public function index() {
+
         $user_selections = userSelections::where('user_id', auth()->id())->get();
-        dd($user_selections);
+        $ticket_price = TicketType::where('id', $user_selections[0]->ticket_type_id)->first()->price * $user_selections[0]->ticket_quantity;
+
+        $checkin_date = \Carbon\Carbon::parse($user_selections[0]->check_in_date);
+        $checkout_date = \Carbon\Carbon::parse($user_selections[0]->check_out_date);
+        $days_stay = $checkin_date->diffInDays($checkout_date);
+
+        $hotel_price = $user_selections[0]->room_price * $days_stay;
+
+        $total_price = $ticket_price + $hotel_price + $user_selections[0]->transport_price;
+
+        return view('checkout');
+
+        dd($total_price);
     }
+
 }
