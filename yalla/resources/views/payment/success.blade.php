d388de43f2f321c158f4670fc16b52907816b875
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-13 22:20:54 +0100

    create the section

--- yalla/resources/views/payment/success.blade.php
+++ yalla/resources/views/payment/success.blade.php
@@ -1,7 +1,10 @@
 <x-head />
 <body class="bg-darkgray text-white font-poppins min-h-screen">
 <x-navbar />
+<!-- Success Section -->
+<section class="py-12 md:py-20 relative overflow-hidden">
 
+</section>
 
 <x-footer />
 </body>
