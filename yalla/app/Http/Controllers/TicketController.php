965570254d16186e4030f81723af3c355dcad314
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-14 11:46:29 +0100

    add the download ticket function

--- yalla/app/Http/Controllers/TicketController.php
+++ yalla/app/Http/Controllers/TicketController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Barryvdh\DomPDF\Facade\Pdf;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use App\Models\Payment;
+use App\Models\userSelections;
 class TicketController extends Controller
 {
-    //
+    public function downloadTicket($paymentId)
+    {
+
+    }
 }
