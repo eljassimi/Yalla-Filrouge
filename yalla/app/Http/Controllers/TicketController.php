00625f112b3c87979657f3182cdbddb7fc9d3fab
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-14 16:55:37 +0100

    complete the download ticket function display the qr code using an free api to generate a qr code

--- yalla/app/Http/Controllers/TicketController.php
+++ yalla/app/Http/Controllers/TicketController.php
@@ -4,25 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use App\Models\Payment;
-use App\Models\userSelections;
+
 class TicketController extends Controller
 {
     public function downloadTicket($paymentId)
     {
         $payment = Payment::findOrFail($paymentId);
-        $userSelection = userSelections::where('user_id', auth()->id())->first();
-
-        $qrCode = base64_encode(QrCode::format('png')->size(200)->generate("UserID: " . auth()->id() . ", SelectionID: " . $userSelection->id));
-
-        $pdf = PDF::loadView('pdf.ticket', [
-            'payment' => $payment,
-            'user' => auth()->user(),
-            'userSelection' => $userSelection,
-            'qrCode' => $qrCode
-        ]);
-
-        return $pdf->download('Your_Ticket.pdf');
+        $user = auth()->user();
+        $data = "User: {$user->name} | PaymentID: {$payment->id}";
+        $qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" . urlencode($data);
+        return view('pdf.ticket',compact('user','payment','data','qrCodeUrl'));
     }
 }
