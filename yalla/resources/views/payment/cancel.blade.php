e07602a96d20042d889a7a29d1a87b55f6d39c6b
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-14 11:22:08 +0100

    add the page title

--- yalla/resources/views/payment/cancel.blade.php
+++ yalla/resources/views/payment/cancel.blade.php
@@ -5,6 +5,16 @@
     <div class="absolute top-0 left-0 w-48 h-48 bg-primary/10 rounded-full -translate-x-1/2 -translate-y-1/2 blur-3xl"></div>
     <div class="absolute bottom-0 right-0 w-64 h-64 bg-primary/10 rounded-full translate-x-1/2 translate-y-1/2 blur-3xl"></div>
 
+    <div class="container mx-auto px-4 max-w-4xl">
+        <div class="text-center mb-12">
+            <p class="text-lightgray uppercase tracking-wider mb-2">PAYMENT CANCELLED</p>
+            <h1 class="text-3xl md:text-4xl font-bold inline-block relative">
+                Your Payment Was Cancelled
+                <span class="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-primary"></span>
+            </h1>
+        </div>
+
+    </div>
 </section>
 </body>
 </html>
