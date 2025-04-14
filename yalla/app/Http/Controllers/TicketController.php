4f7431bea424aeaf9aba7ed0dedb044e6f9e7003
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-14 11:47:38 +0100

    complete the download ticket function

--- yalla/app/Http/Controllers/TicketController.php
+++ yalla/app/Http/Controllers/TicketController.php
@@ -11,6 +11,18 @@ class TicketController extends Controller
 {
     public function downloadTicket($paymentId)
     {
+        $payment = Payment::findOrFail($paymentId);
+        $userSelection = userSelections::where('user_id', auth()->id())->first();
 
+        $qrCode = base64_encode(QrCode::format('png')->size(200)->generate("UserID: " . auth()->id() . ", SelectionID: " . $userSelection->id));
+
+        $pdf = PDF::loadView('pdf.ticket', [
+            'payment' => $payment,
+            'user' => auth()->user(),
+            'userSelection' => $userSelection,
+            'qrCode' => $qrCode
+        ]);
+
+        return $pdf->download('Your_Ticket.pdf');
     }
 }
