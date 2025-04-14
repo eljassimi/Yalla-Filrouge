a58007d7d539fbdd0064d11676081d68f1de6390
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-14 20:28:56 +0100

    add the logo and the ticket number

--- yalla/resources/views/pdf/ticket.blade.php
+++ yalla/resources/views/pdf/ticket.blade.php
@@ -1,5 +1,25 @@
 <x-head />
 <body class="bg-darkgray min-h-screen flex items-center justify-center p-4 font-['Poppins']">
+<div class="max-w-md w-full my-4">
+    <div class="bg-darkgray text-white rounded-2xl overflow-hidden shadow-2xl relative transform transition-transform hover:scale-105">
+        <div class="absolute top-0 left-0 w-full h-2 bg-primary"></div>
 
+        <!-- Ticket Header -->
+        <div class="relative p-6 pb-4">
+            <div class="flex justify-between items-center">
+                <!-- Logo -->
+                <div class="flex items-center">
+                  <img src="assets/logo/Yalla2.png" class="w-24">
+                </div>
+                <!-- Ticket Number -->
+                <div class="text-lightgray text-sm">
+                    #YE2030-M7842
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
 </body>
 </html>
