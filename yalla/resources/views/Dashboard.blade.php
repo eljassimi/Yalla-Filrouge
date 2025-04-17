38f9ad1cea6542c188ddd7a1b83297c5bde46e52
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 10:21:03 +0100

    create a nav bar with a dashboard link

--- yalla/resources/views/Dashboard.blade.php
+++ yalla/resources/views/Dashboard.blade.php
@@ -2,13 +2,24 @@
 <body class="bg-darkgray text-white">
 
 <div class="flex h-screen overflow-hidden">
-
     <aside class="hidden md:flex md:flex-col w-64 bg-darkgray border-r border-gray-700">
         <div class="p-4 border-b border-gray-700">
             <img src="/assets/logo/Yalla2.png" class="w-32">
             <p class="text-sm text-lightgray">Admin Dashboard</p>
         </div>
+        <nav class="flex-1 overflow-y-auto py-4">
+            <ul>
+                <li class="mb-1">
+                    <a href="#"  class="flex items-center px-4 py-3 text-lightgray hover:bg-gray-800 hover:text-white rounded-lg mx-2 transition-colors">
+                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+                        </svg>
+                        Dashboard
+                    </a>
+                </li>
 
+            </ul>
+        </nav>
     </aside>
 
 </div>
