2c5813e0d596a45156b653a11610ccb50214ad59
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 10:24:36 +0100

    create the hotels link inthe navbar

--- yalla/resources/views/Dashboard.blade.php
+++ yalla/resources/views/Dashboard.blade.php
@@ -33,6 +33,14 @@
                         Matches
                     </a>
                 </li>
+                <li class="mb-1">
+                    <a href="#" class="flex items-center px-4 py-3 text-lightgray hover:bg-gray-800 hover:text-white rounded-lg mx-2 transition-colors">
+                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
+                        </svg>
+                        Hotels
+                    </a>
+                </li>
 
             </ul>
         </nav>
