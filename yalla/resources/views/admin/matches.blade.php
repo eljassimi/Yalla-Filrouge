ca98b16952fd61a3fe17a547528eb6ab850bd5bd
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 12:28:15 +0100

    add the title of the page

--- yalla/resources/views/admin/matches.blade.php
+++ yalla/resources/views/admin/matches.blade.php
@@ -6,7 +6,15 @@
     <div class="flex-1 flex flex-col overflow-hidden">
         <x-DashboardHeader />
         <main class="flex-1 overflow-y-auto bg-black p-4">
+            <div id="matches-page" class="page-content">
+                <div class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
+                    <div>
+                        <h1 class="text-2xl font-bold">Match Management</h1>
+                        <p class="text-lightgray">Manage scheduled matches and events</p>
+                    </div>
+                </div>
 
+            </div>
         </main>
     </div>
 </div>
