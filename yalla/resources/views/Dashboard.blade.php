9876f85982f31cc2931dd59e726bdc89bff593be
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 11:05:32 +0100

    add a logo for the mobil version

--- yalla/resources/views/Dashboard.blade.php
+++ yalla/resources/views/Dashboard.blade.php
@@ -69,7 +69,10 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                     </svg>
                 </button>
+                <img src="/assets/logo/Yalla2.png" alt="logo" class=" w-24 ms:hidden">
             </div>
+
+
         </header>
     </div>
 </div>
