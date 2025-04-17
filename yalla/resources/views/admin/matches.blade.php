783d9ba569b3b66e513aede15087fb4cb94f0b21
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 13:37:59 +0100

    create the table for the match management

--- yalla/resources/views/admin/matches.blade.php
+++ yalla/resources/views/admin/matches.blade.php
@@ -20,6 +20,13 @@
                     </button>
                 </div>
 
+                <div class="bg-darkgray rounded-lg shadow overflow-hidden">
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full divide-y divide-gray-700">
+                        </table>
+                    </div>
+                </div>
+
             </div>
         </main>
     </div>
