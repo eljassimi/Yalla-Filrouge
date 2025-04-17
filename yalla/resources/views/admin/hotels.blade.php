0dd1b67e2698edb04b3b62388868f2168e73cab3
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 16:19:26 +0100

    add the aside components and the dashboard header and the main section

--- yalla/resources/views/admin/hotels.blade.php
+++ yalla/resources/views/admin/hotels.blade.php
@@ -1,1 +1,16 @@
 <x-head />
+<body class="bg-darkgray text-white">
+
+<div class="flex h-screen overflow-hidden">
+    <x-aside />
+    <div class="flex-1 flex flex-col overflow-hidden">
+        <x-DashboardHeader />
+        <main class="flex-1 overflow-y-auto bg-black p-4">
+
+        </main>
+    </div>
+</div>
+
+
+</body>
+</html>
