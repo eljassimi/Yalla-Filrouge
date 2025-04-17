93fa9625088159c520b87ca0d6c072b8b0620381
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 16:28:52 +0100

    create the table for hotels management

--- yalla/resources/views/admin/hotels.blade.php
+++ yalla/resources/views/admin/hotels.blade.php
@@ -21,6 +21,24 @@
                     </button>
                 </div>
 
+                <div class="bg-darkgray rounded-lg shadow overflow-hidden">
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full divide-y divide-gray-700">
+                            <thead class="bg-gray-800">
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-lightgray uppercase tracking-wider">Hotel</th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-lightgray uppercase tracking-wider">Description</th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-lightgray uppercase tracking-wider">Rooms</th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-lightgray uppercase tracking-wider">Location</th>
+                                <th scope="col" class="px-6 py-3 text-right text-xs font-medium text-lightgray uppercase tracking-wider">Actions</th>
+                            </tr>
+                            </thead>
+
+                        </table>
+                    </div>
+
+                </div>
+
             </div>
         </main>
     </div>
