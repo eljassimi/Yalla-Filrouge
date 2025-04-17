b0f75bf62eb75ff75f36f65fe6b46a0a14974770
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 10:24:59 +0100

    create the transport link in the navbar

--- yalla/resources/views/Dashboard.blade.php
+++ yalla/resources/views/Dashboard.blade.php
@@ -41,7 +41,14 @@
                         Hotels
                     </a>
                 </li>
-
+                <li class="mb-1">
+                    <a href="#" class="flex items-center px-4 py-3 text-lightgray hover:bg-gray-800 hover:text-white rounded-lg mx-2 transition-colors">
+                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"></path>
+                        </svg>
+                        Transport
+                    </a>
+                </li>
             </ul>
         </nav>
     </aside>
