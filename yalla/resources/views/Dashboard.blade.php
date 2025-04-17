938fb87d4f0290d004578002c9b8cae0abe83aa8
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 10:23:50 +0100

    create the matches link in the navbar

--- yalla/resources/views/Dashboard.blade.php
+++ yalla/resources/views/Dashboard.blade.php
@@ -25,6 +25,14 @@
                         Users
                     </a>
                 </li>
+                <li class="mb-1">
+                    <a href="#" class="flex items-center px-4 py-3 text-lightgray hover:bg-gray-800 hover:text-white rounded-lg mx-2 transition-colors">
+                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                        </svg>
+                        Matches
+                    </a>
+                </li>
 
             </ul>
         </nav>
