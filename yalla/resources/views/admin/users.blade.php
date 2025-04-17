dc2a0ea596cab7bfb96241c2f699e7ebc743fddd
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 12:04:37 +0100

    add a title for the user management page

--- yalla/resources/views/admin/users.blade.php
+++ yalla/resources/views/admin/users.blade.php
@@ -6,7 +6,14 @@
     <div class="flex-1 flex flex-col overflow-hidden">
         <x-DashboardHeader />
         <main class="flex-1 overflow-y-auto bg-black p-4">
-
+            <div id="users-page" class="page-content">
+                <div class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
+                    <div>
+                        <h1 class="text-2xl font-bold">User Management</h1>
+                        <p class="text-lightgray">Manage registered users</p>
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
 </div>
