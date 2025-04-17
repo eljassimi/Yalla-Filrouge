2a3072ca8dd19a5844b4d985904086254b2d560e
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 11:06:45 +0100

    create a search input

--- yalla/resources/views/Dashboard.blade.php
+++ yalla/resources/views/Dashboard.blade.php
@@ -69,7 +69,17 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                     </svg>
                 </button>
-                <img src="/assets/logo/Yalla2.png" alt="logo" class=" w-24 ms:hidden">
+                <img src="/assets/logo/Yalla2.png" alt="logo" class=" w-24 md:hidden">
+
+                <div class="hidden md:flex items-center flex-1 mx-4">
+                    <div class="relative w-full max-w-md">
+                        <input type="text" placeholder="Search..." class="w-full bg-gray-800 text-white rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary">
+                        <svg class="w-5 h-5 text-gray-400 absolute left-3 top-2.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+                </div>
+
             </div>
 
 
