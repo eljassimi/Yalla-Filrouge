ec79c37925c6501322312885c5d32e7252871b3b
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 10:19:59 +0100

    create aside section with the logo of the project

--- yalla/resources/views/Dashboard.blade.php
+++ yalla/resources/views/Dashboard.blade.php
@@ -1,5 +1,18 @@
 <x-head />
 <body class="bg-darkgray text-white">
 
+<div class="flex h-screen overflow-hidden">
+
+    <aside class="hidden md:flex md:flex-col w-64 bg-darkgray border-r border-gray-700">
+        <div class="p-4 border-b border-gray-700">
+            <img src="/assets/logo/Yalla2.png" class="w-32">
+            <p class="text-sm text-lightgray">Admin Dashboard</p>
+        </div>
+
+    </aside>
+
+</div>
+
+
 </body>
 </html>
