a9dc34b29c8742ce0477ab9e3380f50233ac4bc4
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-17 10:23:07 +0100

    create the users link in the navbar

--- yalla/resources/views/Dashboard.blade.php
+++ yalla/resources/views/Dashboard.blade.php
@@ -17,6 +17,14 @@
                         Dashboard
                     </a>
                 </li>
+                <li class="mb-1">
+                    <a href="#" class="flex items-center px-4 py-3 text-lightgray hover:bg-gray-800 hover:text-white rounded-lg mx-2 transition-colors">
+                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
+                        </svg>
+                        Users
+                    </a>
+                </li>
 
             </ul>
         </nav>
