2bd8f06e58a6372fad73a46d23700825689dc082
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-18 14:52:03 +0100

    add the dahsboard header and the main section

--- yalla/resources/views/admin/transports.blade.php
+++ yalla/resources/views/admin/transports.blade.php
@@ -3,7 +3,12 @@
 
 <div class="flex h-screen overflow-hidden">
     <x-aside />
+    <div class="flex-1 flex flex-col overflow-hidden">
+        <x-DashboardHeader />
+        <main class="flex-1 overflow-y-auto bg-black p-4">
 
+        </main>
+    </div>
 </div>
 
 
