f88b29b3d5f51d7609866c061e633f88e1a94455
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-19 20:05:30 +0100

    add OpenStreetMap geosearch provider with country restriction to Morocco

--- yalla/resources/views/admin/matches.blade.php
+++ yalla/resources/views/admin/matches.blade.php
@@ -198,9 +198,16 @@
 
         L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            subdomains: 'abcd',
             maxZoom: 13
         }).addTo(map);
 
+        const provider = new GeoSearch.OpenStreetMapProvider({
+            params: {
+                'countrycodes': 'ma'
+            }
+        });
 
+    });
 </script>
 </body>
