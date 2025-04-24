a9b6021962f9d9f4a8093da846e925863e5e5a80
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-24 21:51:57 +0100

    add titles

--- yalla/resources/views/admin/editHotelForm.blade.php
+++ yalla/resources/views/admin/editHotelForm.blade.php
@@ -7,6 +7,16 @@
     <x-aside />
     <div class="flex-1 flex flex-col overflow-hidden">
         <x-DashboardHeader />
+
+        <main class="flex-1 overflow-y-auto bg-black p-4 md:p-8">
+
+            <div id="edit-hotel-page">
+                <div class="mb-6">
+                    <h1 class="text-2xl font-bold">Edit Hotel</h1>
+                    <p class="text-lightgray">Update hotel details and room information</p>
+                </div>
+            </div>
+        </main>
     </div>
 </div>
 
