b4518015f4d6e681751ed47d391a4a7acc23889b
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-24 21:51:17 +0100

    add the side nav and the dashboard header

--- yalla/resources/views/admin/editHotelForm.blade.php
+++ yalla/resources/views/admin/editHotelForm.blade.php
@@ -3,5 +3,12 @@
 <x-head />
 <body class="bg-black text-white">
 
+<div class="flex h-screen overflow-hidden">
+    <x-aside />
+    <div class="flex-1 flex flex-col overflow-hidden">
+        <x-DashboardHeader />
+    </div>
+</div>
+
 </body>
 </html>
